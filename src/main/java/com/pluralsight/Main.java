d3d51e42f9b498a678889c4af37f25049a7adff8
RickelviC <Cruzrickelvi@gmail.com>
2026-04-21 14:56:55 -0400

    added time and dates

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static void main(String[] args) {
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+
+        LocalDateTime today = LocalDateTime.now();
+
+        LocalDate date = LocalDate.now();
+
+
+
+        String formattedDate = today.format(dateTimeFormatter);
+
+        System.out.println(formattedDate);
+        System.out.println(date);
+        System.out.println(date.getDayOfWeek() + " " + date.getDayOfMonth() + ", " + date.getYear());
+        System.out.println(date.getDayOfWeek() + ", " + date.getMonth() +" " + date.getDayOfMonth()
+                + ", " + date.getYear() + " " + today.getHour() + ":" + today.getMinute());// not in GMT
+
+        System.out.println(today.getHour() + ":" + today.getMinute() + " on " + date.getDayOfMonth() );
+
+    }
 }
